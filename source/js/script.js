923fe7fc9c14830bffd58e674bd0784d408e2051
wergut <35626608+wergut@users.noreply.github.com>
2024-04-01 19:23:50 +0500

    New project

---
+++ source/js/script.js
@@ -0,0 +1,104 @@
+var swiper1 = new Swiper(".testimonials-slider", {
+  observer: true,
+  observeParents: true,
+  observeSlideChildren: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".testimonials-slider .swiper-button-next",
+    prevEl: ".testimonials-slider .swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    // when window width is >= 480px
+    601: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  }
+});
+
+var swiper2 = new Swiper(".cases-slider", {
+  observer: true,
+  observeParents: true,
+  observeSlideChildren: true,
+  slidesPerView: 1,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: ".cases-slider .swiper-button-next",
+    prevEl: ".cases-slider .swiper-button-prev",
+  },
+  pagination: {
+    el: ".cases-slider .swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
+});
+
+var swiper3 = new Swiper(".staff-slider", {
+  observer: true,
+  observeParents: true,
+  observeSlideChildren: true,
+  slidesPerView: 4,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: ".staff-slider .swiper-button-next",
+    prevEl: ".staff-slider .swiper-button-prev",
+  },
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const accordionItems = document.querySelectorAll('.accordion-item');
+
+  if (accordionItems) {
+    accordionItems.forEach(item => {
+      const trigger = item.querySelector('.accordion-item-header');
+      const content = item.querySelector('.accordion-item-content');
+
+      trigger.addEventListener('click', function() {
+        const parent = this.parentNode;
+
+        if (parent.classList.contains('active')) {
+          parent.classList.remove('active');
+          content.style.height = '0';
+        } else {
+          document.querySelectorAll('.accordion-item').forEach(child => {
+            child.classList.remove('active');
+            child.querySelector('.accordion-item-content').style.height = '0';
+          });
+          parent.classList.add('active');
+          content.style.height = content.scrollHeight + 'px';
+        }
+      });
+    });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var filters = document.querySelectorAll('.filter');
+
+  filters.forEach(function(filter) {
+    filter.addEventListener('click', function() {
+      filters.forEach(function(otherFilter) {
+        if (otherFilter !== filter) {
+          otherFilter.classList.remove('active');
+        }
+      });
+      filter.classList.add('active');
+    });
+  });
+});
+
