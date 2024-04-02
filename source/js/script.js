6033c1fae85e95fe45bbee72cdac6a019a4fbe77
wergut <35626608+wergut@users.noreply.github.com>
2024-04-02 18:21:46 +0500

    update

--- source/js/script.js
+++ source/js/script.js
@@ -12,11 +12,11 @@ var swiper1 = new Swiper(".testimonials-slider", {
     // when window width is >= 320px
     320: {
       slidesPerView: 1,
-      spaceBetween: 0,
+      spaceBetween: 12,
     },
     // when window width is >= 480px
     601: {
-      slidesPerView: 2,
+      slidesPerView: 1.5,
       spaceBetween: 24,
     },
     1024: {
@@ -88,6 +88,33 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
+  const collapseItems = document.querySelectorAll('.case-info-item');
+
+  if (collapseItems) {
+    collapseItems.forEach(item => {
+      const trigger = item.querySelector('.case-info-header');
+      const content = item.querySelector('.case-info-content');
+
+      trigger.addEventListener('click', function() {
+        const parent = this.parentNode;
+
+        if (parent.classList.contains('active')) {
+          parent.classList.remove('active');
+          content.style.height = '0';
+        } else {
+          document.querySelectorAll('.case-info-item').forEach(child => {
+            child.classList.remove('active');
+            child.querySelector('.case-info-content').style.height = '0';
+          });
+          parent.classList.add('active');
+          content.style.height = content.scrollHeight + 'px';
+        }
+      });
+    });
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
   var filters = document.querySelectorAll('.filter');
 
   filters.forEach(function(filter) {
@@ -101,4 +128,20 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const btnMenuMobile = document.querySelector('.mobile-menu-btn');
+  const headerMobileWrapper = document.querySelector('.main-menu');
+
+  btnMenuMobile.addEventListener('click', function () {
+    headerMobileWrapper.classList.toggle('show');
+
+    if (headerMobileWrapper.classList.contains('show')) {
+      btnMenuMobile.classList.add('cross');
+    } else {
+      btnMenuMobile.classList.remove('cross');
+    }
+  });
+});
+
 
