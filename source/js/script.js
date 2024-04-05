d5e6781969ba3f65e8cdcbf8049ac4f4e67f917c
wergut <35626608+wergut@users.noreply.github.com>
2024-04-05 14:18:50 +0500

    update

--- source/js/script.js
+++ source/js/script.js
@@ -237,4 +237,12 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+let element = document.querySelectorAll('input[class="phone"]');
+let maskOptions = {
+  mask: '+{7}(000)000-00-00'
+};
+for (let i = 0; i < element.length; i++){
+  let mask = IMask(element[i], maskOptions);
+}
+
 
